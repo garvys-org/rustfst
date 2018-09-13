34cf20bbd98bb32cd373c8a190c82120bb067564
Garvys <alexandre.caulier@protonmail.com>
2018-09-14 01:15:47 +0200

    Add probability semiring

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+pub mod semiring;
+
 #[cfg(test)]
 mod tests {
     #[test]
---
+++ src/semiring.rs
@@ -0,0 +1,33 @@
+pub trait Semiring {
+    fn plus(&self, rhs: &Self) -> Self;
+    fn times(&self, rhs: &Self) -> Self;
+    fn zero() -> Self;
+    fn one() -> Self;
+}
+
+pub struct ProbabilitySemiring {
+    value: f32
+}
+
+impl ProbabilitySemiring {
+    pub fn new(value: f32) -> Self {
+        ProbabilitySemiring {value}
+    }
+}
+
+impl Semiring for ProbabilitySemiring {
+    fn plus(&self, rhs: &Self) -> Self {
+        Self::new(self.value + rhs.value)
+    }
+    fn times(&self, rhs: &Self) -> Self {
+        Self::new(self.value * rhs.value)
+    }
+
+    fn zero() -> Self {
+        Self::new(0.0)
+    }
+
+    fn one() -> Self {
+        Self::new(1.0)
+    }
+}
