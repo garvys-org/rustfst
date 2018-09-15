4430bb9c0f90be064b508f9a4e0e3be9f443e910
Garvys <alexandre.caulier@orange.fr>
2018-09-15 13:45:46 +0200

    Add traits MutableFst and ExpandedFst

--- src/fst.rs
+++ src/fst.rs
@@ -1,5 +1,6 @@
 use arc::Arc;
 use StateId;
+use Label;
 use semirings::Semiring;
 
 pub trait Fst<W: Semiring> {
@@ -12,4 +13,21 @@ pub trait Fst<W: Semiring> {
     //fn get_isyms(&self) -> Option<Self::Symtab>;
     //fn get_osyms(&self) -> Option<Self::Symtab>;
     fn is_final(&self, &StateId) -> bool;
+    fn num_arcs(&self) -> usize;
+}
+
+pub trait MutableFst<W: Semiring>: Fst<W> {
+    fn new() -> Self;
+    fn set_start(&mut self, id: StateId);
+    fn add_state(&mut self, finalweight: W) -> StateId;
+    fn del_state(&mut self, StateId);
+    fn del_states<T: IntoIterator<Item=StateId>>(&mut self, states: T);
+    fn add_arc(&mut self, source: StateId, target: StateId, ilabel: Label, olabel: Label, weight: W);
+    fn set_finalweight(&mut self, id: StateId, finalweight: W);
+    // fn set_isyms<T: IntoIterator<Item=String>>(&mut self, symtab: T);
+    // fn set_osyms<T: IntoIterator<Item=String>>(&mut self, symtab: T);
+}
+
+pub trait ExpandedFst<W: Semiring>: Fst<W> {
+    fn get_numstates(&self) -> usize;
 }
