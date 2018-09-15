d051ffcf7d80cd5c88a535886ec1b99ff7859365
Garvys <alexandre.caulier@protonmail.com>
2018-09-15 13:01:28 +0200

    Add fst.rs

---
+++ src/fst.rs
@@ -0,0 +1,15 @@
+use arc::Arc;
+use StateId;
+use semiring::Semiring;
+
+pub trait Fst<W: Semiring> {
+    type Arc: Arc<W>;
+    type Iter: Iterator<Item=Self::Arc>;
+    //type Symtab: IntoIterator<Item=String>;
+    fn start(&self) -> Option<StateId>;
+    fn final_weight(&self, StateId) -> W;
+    fn arc_iter(&self, StateId) -> Self::Iter;
+    //fn get_isyms(&self) -> Option<Self::Symtab>;
+    //fn get_osyms(&self) -> Option<Self::Symtab>;
+    fn is_final(&self, StateId) -> bool;
+}
