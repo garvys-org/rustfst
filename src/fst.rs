d4f707db84619e2c602ac965dc62635122a32447
Garvys <alexandre.caulier@orange.fr>
2018-09-17 00:23:19 +0200

    Implement transducer function

--- src/fst.rs
+++ src/fst.rs
@@ -37,3 +37,28 @@ pub trait MutableFst<W: Semiring>: Fst<W> {
 pub trait ExpandedFst<W: Semiring>: Fst<W> {
     fn num_states(&self) -> usize;
 }
+
+use std::cmp;
+pub fn transducer<T: Iterator<Item = Label>, W: Semiring, F: MutableFst<W>>(labels_input: T, labels_output: T) -> F {
+    let mut vec_labels_input: Vec<_> = labels_input.collect();
+    let mut vec_labels_output: Vec<_> = labels_output.collect();
+
+    let max_size = cmp::max(vec_labels_input.len(), vec_labels_output.len());
+
+    vec_labels_input.resize(max_size, 0);
+    vec_labels_output.resize(max_size, 0);
+
+    let mut fst = F::new();
+    let mut state_cour = fst.add_state();
+    fst.set_start(&state_cour);
+
+    for (i, o) in vec_labels_input.iter().zip(vec_labels_output.iter()) {
+        let new_state = fst.add_state();
+        fst.add_arc(&state_cour, &new_state, *i, *o, W::zero());
+        state_cour = new_state;
+    }
+
+    fst.set_final(&state_cour, W::one());
+
+    fst
+}
