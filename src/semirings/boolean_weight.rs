a47e2455e49213f209027738ae199bdb4acc70e9
Garvys <alexandre.caulier@orange.fr>
2018-09-23 21:13:14 +0200

    Implement BooleanWeight

---
+++ src/semirings/boolean_weight.rs
@@ -0,0 +1,53 @@
+use semirings::Semiring;
+
+#[derive(Clone, Debug, PartialEq, Default, Eq)]
+pub struct BooleanWeight {
+    value: bool,
+}
+
+impl BooleanWeight {
+    pub fn new(value: bool) -> Self {
+        BooleanWeight { value }
+    }
+}
+
+impl Semiring for BooleanWeight {
+    fn plus(&self, rhs: &Self) -> Self {
+        Self::new(self.value | rhs.value)
+    }
+    fn times(&self, rhs: &Self) -> Self {
+        Self::new(self.value & rhs.value)
+    }
+
+    fn zero() -> Self {
+        Self::new(false)
+    }
+
+    fn one() -> Self {
+        Self::new(true)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_boolean_weight() {
+        let b_true = BooleanWeight::new(true);
+        let b_false = BooleanWeight::new(false);
+
+        // Test plus
+        assert_eq!(b_true.plus(&b_true), b_true);
+        assert_eq!(b_true.plus(&b_false), b_true);
+        assert_eq!(b_false.plus(&b_true), b_true);
+        assert_eq!(b_false.plus(&b_false), b_false);
+
+        // Test times
+        assert_eq!(b_true.times(&b_true), b_true);
+        assert_eq!(b_true.times(&b_false), b_false);
+        assert_eq!(b_false.times(&b_true), b_false);
+        assert_eq!(b_false.times(&b_false), b_false);
+
+    }
+}
--- src/semirings/mod.rs
+++ src/semirings/mod.rs
@@ -1,7 +1,9 @@
 mod semiring;
 mod probability_weight;
 mod tropical_weight;
+mod boolean_weight;
 
 pub use self::semiring::{Semiring, WeaklyDivisibleSemiring};
 pub use self::probability_weight::ProbabilityWeight;
 pub use self::tropical_weight::TropicalWeight;
+pub use self::boolean_weight::BooleanWeight;
