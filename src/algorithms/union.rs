99333d92d087dd375a29f168d44ee2a3f82cf72f
Garvys <alexandre.caulier@orange.fr>
2018-09-24 22:56:30 +0200

    Implement Union operation

---
+++ src/algorithms/union.rs
@@ -0,0 +1,86 @@
+use fst::MutableFst;
+use fst::ExpandedFst;
+use fst::CoreFst;
+use semirings::Semiring;
+use std::collections::HashMap;
+use StateId;
+use fst::FinalStateIterator;
+
+fn add_epsilon_arc_to_initial_state<F1, F2>(fst: &F1, mapping: &HashMap<StateId, StateId>, fst_out: &mut F2)
+where  
+    F1: ExpandedFst,
+    F2: MutableFst,
+{
+    let start_state = fst_out.start().unwrap();
+    if let Some(old_start_state_fst) = fst.start() {
+        fst_out.add_arc(
+            &start_state,
+            mapping.get(&old_start_state_fst).unwrap(),
+            0,
+            0,
+            <F2 as CoreFst>::W::one(),
+        );
+    }
+}
+
+fn set_new_final_states<W, F1, F2>(fst: &F1, mapping: &HashMap<StateId, StateId>, fst_out: &mut F2)
+where  
+    W: Semiring,
+    F1: ExpandedFst<W=W>,
+    F2: MutableFst<W=W>,
+{
+    for old_final_state in fst.final_states_iter() {
+        let final_state = mapping.get(&old_final_state).unwrap();
+        fst_out.set_final(final_state, fst.final_weight(&old_final_state).unwrap());
+    }
+}
+
+pub fn union<W, F1, F2, F3>(fst_1: &F1, fst_2: &F2) -> F3
+where 
+    W: Semiring,
+    F1: ExpandedFst<W=W>,
+    F2: ExpandedFst<W=W>,
+    F3: MutableFst<W=W>,
+{
+    let mut fst_out = F3::new();
+
+    let start_state = fst_out.add_state();
+    fst_out.set_start(&start_state);
+
+    let mapping_states_fst_1 = fst_out.add_fst(fst_1);
+    let mapping_states_fst_2 = fst_out.add_fst(fst_2);
+
+    add_epsilon_arc_to_initial_state(fst_1, &mapping_states_fst_1, &mut fst_out);
+    add_epsilon_arc_to_initial_state(fst_2, &mapping_states_fst_2, &mut fst_out);
+
+    set_new_final_states(fst_1, &mapping_states_fst_1, &mut fst_out);
+    set_new_final_states(fst_2, &mapping_states_fst_2, &mut fst_out);
+
+    fst_out
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use vector_fst::VectorFst;
+    use semirings::BooleanWeight;
+    use fst::FinalStateIterator;
+    use fst::transducer;
+
+    #[test]
+    fn test_union() {
+        let v_a_1 = vec![1, 2, 3];
+        let v_a_2 = vec![4, 5, 6];
+
+        let v_b_1 = vec![10, 20, 30];
+        let v_b_2 = vec![40, 50, 60];
+
+        let t_a : VectorFst<BooleanWeight> = transducer(v_a_1.clone().into_iter(), v_a_2.clone().into_iter());
+        let t_b : VectorFst<BooleanWeight> = transducer(v_b_1.clone().into_iter(), v_b_2.into_iter());
+
+        let new_fst : VectorFst<_> = union(&t_a, &t_b);
+
+        println!("{:?}", new_fst);
+    }
+
+}
