84cca6742d0dd1ee7fd21bc6550414b3d1f77a8e
Garvys <alexandre.caulier@orange.fr>
2018-10-02 22:37:27 +0200

    Add doc tests Arc + added set_value

--- src/arc.rs
+++ src/arc.rs
@@ -2,6 +2,7 @@ use semirings::Semiring;
 use Label;
 use StateId;
 
+/// Arc structure.
 #[derive(Debug, Clone, PartialEq)]
 pub struct Arc<W: Semiring> {
     pub ilabel: Label,
@@ -11,6 +12,23 @@ pub struct Arc<W: Semiring> {
 }
 
 impl<W: Semiring> Arc<W> {
+
+    /// Creates a new Arc.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use rustfst::arc::Arc;
+    /// use rustfst::semirings::{BooleanWeight, Semiring};
+    /// 
+    /// let arc = Arc::new(0, 1, BooleanWeight::one(), 2);
+    /// 
+    /// assert_eq!(arc.ilabel, 0);
+    /// assert_eq!(arc.olabel, 1);
+    /// assert_eq!(arc.weight, BooleanWeight::one());
+    /// assert_eq!(arc.nextstate, 2);
+    ///
+    /// ```
     pub fn new(ilabel: Label, olabel: Label, weight: W, nextstate: StateId) -> Self {
         Arc {
             ilabel,
@@ -18,5 +36,28 @@ impl<W: Semiring> Arc<W> {
             weight,
             nextstate,
         }
+    }
+
+    /// Updates the values of the attributes of the Arc from another Arc.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use rustfst::arc::Arc;
+    /// use rustfst::semirings::{BooleanWeight, Semiring};
+    /// 
+    /// let mut arc_1 = Arc::new(0, 1, BooleanWeight::one(), 2);
+    /// let arc_2 = Arc::new(1, 2, BooleanWeight::zero(), 3);
+    ///
+    /// arc_1.set_value(&arc_2);
+    ///
+    /// assert_eq!(arc_1, arc_2);
+    ///
+    /// ```
+    pub fn set_value(&mut self, arc: &Arc<W>) {
+        self.ilabel = arc.ilabel;
+        self.olabel = arc.olabel;
+        self.weight = arc.weight.clone();
+        self.nextstate = arc.nextstate;
     }
 }
