5fa408eae5ace0d66ce968dcb75cced360192d22
Garvys <alexandre.caulier@orange.fr>
2018-10-22 23:47:05 +0200

    Doc test shortest_distance

--- src/algorithms/single_source_shortest_distance.rs
+++ src/algorithms/single_source_shortest_distance.rs
@@ -4,9 +4,39 @@ use std::collections::VecDeque;
 use Result;
 use StateId;
 
+/// This operation computes the shortest distance from the the state `state_id` to every state.
+/// The shortest distance from p to q is the ⊕-sum of the weights of all the paths between p and q.
+///
+/// # Example
+/// ```
+/// use rustfst::semirings::{Semiring, IntegerWeight};
+/// use rustfst::fst_impls::VectorFst;
+/// use rustfst::fst_traits::MutableFst;
+/// use rustfst::algorithms::single_source_shortest_distance;
+/// use rustfst::arc::Arc;
+///
+/// let mut fst = VectorFst::new();
+/// let s0 = fst.add_state();
+/// let s1 = fst.add_state();
+/// let s2 = fst.add_state();
+///
+/// fst.set_start(&s0).unwrap();
+/// fst.add_arc(&s0, Arc::new(32, 23, IntegerWeight::new(18), s1));
+/// fst.add_arc(&s0, Arc::new(32, 23, IntegerWeight::new(21), s2));
+/// fst.add_arc(&s1, Arc::new(32, 23, IntegerWeight::new(55), s2));
+///
+/// let dists = single_source_shortest_distance(&fst, s1).unwrap();
+///
+/// assert_eq!(dists, vec![
+///     IntegerWeight::zero(),
+///     IntegerWeight::one(),
+///     IntegerWeight::new(55),
+/// ]);
+///
+/// ```
 pub fn single_source_shortest_distance<F: ExpandedFst>(
     fst: &F,
-    state: StateId,
+    state_id: StateId,
 ) -> Result<Vec<<F as CoreFst>::W>> {
     let num_states = fst.num_states();
 
@@ -17,12 +47,12 @@ pub fn single_source_shortest_distance<F: ExpandedFst>(
     r.resize(num_states, <F as CoreFst>::W::zero());
 
     // Check whether the wFST contains the state
-    if state < fst.num_states() {
-        d[state] = <F as CoreFst>::W::one();
-        r[state] = <F as CoreFst>::W::one();
+    if state_id < fst.num_states() {
+        d[state_id] = <F as CoreFst>::W::one();
+        r[state_id] = <F as CoreFst>::W::one();
 
         let mut queue = VecDeque::new();
-        queue.push_back(state);
+        queue.push_back(state_id);
 
         while !queue.is_empty() {
             let state_cour = queue.pop_front().unwrap();
@@ -45,6 +75,36 @@ pub fn single_source_shortest_distance<F: ExpandedFst>(
     Ok(d)
 }
 
+/// This operation computes the shortest distance from the initial state to every state.
+/// The shortest distance from p to q is the ⊕-sum of the weights of all the paths between p and q.
+///
+/// # Example
+/// ```
+/// use rustfst::semirings::{Semiring, IntegerWeight};
+/// use rustfst::fst_impls::VectorFst;
+/// use rustfst::fst_traits::MutableFst;
+/// use rustfst::algorithms::shortest_distance;
+/// use rustfst::arc::Arc;
+///
+/// let mut fst = VectorFst::new();
+/// let s0 = fst.add_state();
+/// let s1 = fst.add_state();
+/// let s2 = fst.add_state();
+///
+/// fst.set_start(&s0).unwrap();
+/// fst.add_arc(&s0, Arc::new(32, 23, IntegerWeight::new(18), s1));
+/// fst.add_arc(&s0, Arc::new(32, 23, IntegerWeight::new(21), s2));
+/// fst.add_arc(&s1, Arc::new(32, 23, IntegerWeight::new(55), s2));
+///
+/// let dists = shortest_distance(&fst).unwrap();
+///
+/// assert_eq!(dists, vec![
+///     IntegerWeight::one(),
+///     IntegerWeight::new(18),
+///     IntegerWeight::new(21 + 18*55),
+/// ]);
+///
+/// ```
 pub fn shortest_distance<F: ExpandedFst>(fst: &F) -> Result<Vec<<F as CoreFst>::W>> {
     if let Some(start_state) = fst.start() {
         return single_source_shortest_distance(fst, start_state);
