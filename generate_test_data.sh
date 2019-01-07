72a792372c10fe16ad4b3d530ef19d3fca9a0a87
Garvys <alexandre.caulier@orange.fr>
2019-01-07 21:57:36 +0100

    Add script to generate data from pynini

---
+++ generate_test_data.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+set -e
+
+python fst-test-data/fst_test_data/fst_000/gen_data.py
+python fst-test-data/fst_test_data/fst_001/gen_data.py
+python fst-test-data/fst_test_data/fst_002/gen_data.py
+python fst-test-data/fst_test_data/fst_003/gen_data.py
+python fst-test-data/fst_test_data/fst_004/gen_data.py
