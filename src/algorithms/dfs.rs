4431ee364fe4e415d54d0570382d1c28a0e6a13e
Garvys <alexandre.caulier@orange.fr>
2019-04-05 01:17:16 +0200

    Implement tarjan algorithm for finding strongly connected components in a directed graph

--- src/algorithms/dfs.rs
+++ src/algorithms/dfs.rs
@@ -2,7 +2,7 @@ use std::collections::HashSet;
 
 use failure::Fallible;
 
-use crate::fst_traits::Fst;
+use crate::fst_traits::{Fst, ExpandedFst};
 use crate::StateId;
 
 
@@ -32,3 +32,92 @@ pub fn dfs<F: Fst>(
 
     Ok(())
 }
+
+
+struct TarjanAlgorithm<'a, F: Fst> {
+    index: usize,
+    stack: Vec<StateId>,
+    state_id_to_lol: Vec<Option<Lol>>,
+    fst: &'a F,
+    next_scc_id: usize
+}
+
+impl<'a, F: Fst> TarjanAlgorithm<'a, F> {
+    pub fn new(fst: &'a F) -> Self {
+        Self {
+            index: 0,
+            stack: vec![],
+            state_id_to_lol: vec![],
+            fst,
+            next_scc_id: 0
+        }
+    }
+
+    pub fn compute(&mut self) -> Fallible<()> {
+        let states : Vec<_> = self.fst.states_iter().collect();
+        self.state_id_to_lol.resize(states.len(), None);
+        for state in states {
+            if self.state_id_to_lol[state].is_none() {
+                self.strongconnect(state)?;
+            }
+        }
+        Ok(())
+    }
+
+    fn strongconnect(&mut self, state: StateId) -> Fallible<()> {
+
+        let lol = Lol {
+            index: self.index,
+            lowlink: self.index,
+            on_stack: true,
+            scc: None,
+        };
+        self.index += 1;
+        self.stack.push(state);
+        self.state_id_to_lol[state] = Some(lol);
+
+        // Consider successors of v
+        for arc in self.fst.arcs_iter(state)? {
+            if self.state_id_to_lol[arc.nextstate].is_none() {
+                self.strongconnect(arc.nextstate)?;
+                let v_lowlink = self.state_id_to_lol[state].as_ref().unwrap().lowlink;
+                let w_lowlink = self.state_id_to_lol[arc.nextstate].as_ref().unwrap().lowlink;
+                self.state_id_to_lol[state].as_mut().unwrap().lowlink = v_lowlink.min(w_lowlink);
+            } else {
+                let w = self.state_id_to_lol[arc.nextstate].as_ref().unwrap();
+                if w.on_stack {
+                    // Successor w is in stack S and hence in the current SCC
+                    // If w is not on stack, then (v, w) is a cross-edge in the DFS tree and must be ignored
+                    // Note: The next line may look odd - but is correct.
+                    // It says w.index not w.lowlink; that is deliberate and from the original paper]
+                    let v_lowlink = self.state_id_to_lol[state].as_ref().unwrap().lowlink;
+                    let w_index = self.state_id_to_lol[arc.nextstate].as_ref().unwrap().index;
+                    self.state_id_to_lol[state].as_mut().unwrap().lowlink = v_lowlink.min(w_index);
+                }
+            }
+        }
+        let v = self.state_id_to_lol[state].as_ref().unwrap();
+        if v.lowlink == v.index {
+            loop {
+                let w_state_id = self.stack.pop().unwrap();
+                self.state_id_to_lol[w_state_id].as_mut().unwrap().on_stack = false;
+                self.state_id_to_lol[w_state_id].as_mut().unwrap().scc = Some(self.next_scc_id);
+                if w_state_id == state {
+                    break
+                }
+            }
+            self.next_scc_id += 1;
+        }
+
+        Ok(())
+    }
+}
+
+#[derive(Clone)]
+struct Lol {
+    index: StateId,
+    lowlink: StateId,
+    on_stack: bool,
+    scc: Option<usize>
+}
+
