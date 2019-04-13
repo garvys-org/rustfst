d4e70c923ea54f668583119ae4ece9a769103662
Garvys <alexandre.caulier@orange.fr>
2019-04-14 01:33:37 +0200

    Implement StateOrderQueue

--- rustfst/src/algorithms/queues/mod.rs
+++ rustfst/src/algorithms/queues/mod.rs
@@ -1,11 +1,13 @@
+mod auto_queue;
 mod fifo_queue;
 mod lifo_queue;
-mod shortest_first_queue;
-mod trivial_queue;
+mod state_order_queue;
 mod top_order_queue;
+mod trivial_queue;
 
+pub use self::auto_queue::AutoQueue;
 pub use self::fifo_queue::FifoQueue;
 pub use self::lifo_queue::LifoQueue;
-pub use self::shortest_first_queue::ShortestFirstQueue;
+pub use self::state_order_queue::StateOrderQueue;
 pub use self::top_order_queue::TopOrderQueue;
 pub use self::trivial_queue::TrivialQueue;
