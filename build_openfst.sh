14d6adc0f486ddbf8cf0bfd9d846fe39f7398b30
Garvys <alexandre.caulier@orange.fr>
2019-05-26 20:11:04 +0200

    Scripts to fetch/build and run openfst

---
+++ build_openfst.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -e
+
+if [ ! -d openfst ]; then
+    wget http://www.openfst.org/twiki/pub/FST/FstDownload/openfst-1.7.2.tar.gz
+    tar -zxvf openfst-1.7.2.tar.gz
+fi
+
+cd openfst-1.7.2
+./configure --prefix=`pwd` --disable-bin --enable-static
+make
+make install
--- run_openfst.sh
+++ run_openfst.sh
@@ -4,7 +4,7 @@ set -e
 cd rustfst-tests-openfst
 rm **/metadata.json || true
 echo "Compiling..."
-g++ main.cpp -I ../openfst-1.7.2/src/include/ ../openfst-1.7.2/lib/libfst.a
+g++ -std=c++11 main.cpp -I ../openfst-1.7.2/src/include/ ../openfst-1.7.2/lib/libfst.a
 echo "OK"
 echo "Running..."
 ./a.out
