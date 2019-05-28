0735881e4845df9279996323dda5f2084449e77a
Garvys <alexandre.caulier@orange.fr>
2019-05-28 23:30:17 +0200

    Introduce BinarySerializer trait and implement it for VectorFst

--- rustfst/src/fst_traits/mod.rs
+++ rustfst/src/fst_traits/mod.rs
@@ -1,6 +1,7 @@
 #[macro_use]
 mod macros;
 mod binary_parser;
+mod binary_serializer;
 mod expanded_fst;
 mod final_states_iterator;
 mod fst;
@@ -9,6 +10,7 @@ mod paths_iterator;
 mod text_parser;
 
 pub use self::binary_parser::BinaryParser;
+pub use self::binary_serializer::BinarySerializer;
 pub use self::expanded_fst::ExpandedFst;
 pub use self::final_states_iterator::FinalStatesIterator;
 pub use self::fst::{ArcIterator, CoreFst, Fst, StateIterator};
