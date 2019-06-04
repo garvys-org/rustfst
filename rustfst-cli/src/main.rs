7a292b73bc9f73c976a61b3966b9687c3fce1b36
Garvys <alexandre.caulier@orange.fr>
2019-06-05 00:46:37 +0200

    Add ArcSort cli

--- rustfst-cli/src/main.rs
+++ rustfst-cli/src/main.rs
@@ -6,6 +6,7 @@ use log::error;
 
 use crate::pretty_errors::ExitFailure;
 
+mod arcsort;
 mod connect;
 mod minimize;
 mod pretty_errors;
@@ -19,42 +20,67 @@ fn main() {
 
     // Minimization
     let minimize_cmd = SubCommand::with_name("minimize")
-        .about("Minimization algorithm")
+        .about("Minimization algorithm.")
         .version("1.0")
         .author("Alexandre Caulier <alexandre.caulier@protonmail.com>")
         .arg(
             Arg::with_name("in.fst")
-                .help("Path to input fst file")
+                .help("Path to input fst file.")
                 .required(true),
         )
         .arg(
-            Arg::with_name("allow-nondet")
+            Arg::with_name("allow_nondet")
                 .help("Minimize non-deterministic FSTs ?")
-                .long("allow-nondet"),
+                .long("allow_nondet"),
         )
         .arg(
             Arg::with_name("out.fst")
-                .help("Path to output fst file")
+                .help("Path to output fst file.")
                 .required(true),
         );
     app = app.subcommand(minimize_cmd);
 
     // Connect
     let connect_cmd = SubCommand::with_name("connect")
-        .about("Connect algorithm")
+        .about("Connect algorithm.")
         .version("1.0")
         .author("Alexandre Caulier <alexandre.caulier@protonmail.com>")
         .arg(
             Arg::with_name("in.fst")
-                .help("Path to input fst file")
+                .help("Path to input fst file.")
                 .required(true),
         )
         .arg(
             Arg::with_name("out.fst")
-                .help("Path to output fst file")
+                .help("Path to output fst file.")
                 .required(true),
         );
     app = app.subcommand(connect_cmd);
+
+    // Arcsort
+    let arcsort_cmd = SubCommand::with_name("arcsort")
+        .about("Arcsort algorithm.")
+        .version("1.0")
+        .author("Alexandre Caulier <alexandre.caulier@protonmail.com>")
+        .arg(
+            Arg::with_name("in.fst")
+                .help("Path to input fst file.")
+                .required(true),
+        )
+        .arg(
+            Arg::with_name("out.fst")
+                .help("Path to output fst file.")
+                .required(true),
+        )
+        .arg(
+            Arg::with_name("sort_type")
+                .help("Comparison method.")
+                .long("sort_type")
+                .takes_value(true)
+                .possible_values(&["ilabel", "olabel"])
+                .default_value("ilabel"),
+        );
+    app = app.subcommand(arcsort_cmd);
 
     let matches = app.get_matches();
 
@@ -75,11 +101,16 @@ fn handle(matches: clap::ArgMatches) -> Result<(), ExitFailure> {
     match matches.subcommand() {
         ("minimize", Some(m)) => crate::minimize::minimize_cli(
             m.value_of("in.fst").unwrap(),
-            m.is_present("allow-nondet"),
+            m.is_present("allow_nondet"),
             m.value_of("out.fst").unwrap(),
         ),
         ("connect", Some(m)) => crate::connect::connect_cli(
             m.value_of("in.fst").unwrap(),
+            m.value_of("out.fst").unwrap(),
+        ),
+        ("arcsort", Some(m)) => crate::arcsort::arcsort_cli(
+            m.value_of("in.fst").unwrap(),
+            m.value_of("sort_type").unwrap(),
             m.value_of("out.fst").unwrap(),
         ),
         (s, _) => Err(format_err!("Unknown subcommand {}.", s)),
