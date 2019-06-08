30eec04f2b569a64a52df4bcf2c5046d1f33c663
Garvys <alexandre.caulier@orange.fr>
2019-06-09 01:33:30 +0200

    Bash scripts to compare bench rustfst and openfst

---
+++ build_bench.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+cd openfst_benchmark
+
+for file in `ls *.cpp`
+do
+    echo "Building $file"
+    g++ -O3 -std=c++11 $file -I ../openfst-1.7.2/src/include/ ../openfst-1.7.2/lib/libfst.a -o "${file%.*}"
+done
+
+cd ..
+
+cargo build --release -p rustfst-cli
+
---
+++ run_bench.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [ -d tmp_benchs ]; then
+    rm -r tmp_benchs
+fi
+
+mkdir tmp_benchs
+
+echo "==================================================================="
+echo "BENCH INVERT"
+echo "C++ :"
+./openfst_benchmark/bench_invert 3 10 $1 tmp_benchs/invert_openfst.fst
+echo "RUST :"
+./target/release/rustfst-cli invert $1 tmp_benchs/invert_rustfst.fst --bench --n_warm_ups 3 --n_iters 10
+echo "==================================================================="
+echo -e "\n\n"
+
+echo "==================================================================="
+echo "BENCH PROJECT"
+echo "C++ :"
+./openfst_benchmark/bench_project 3 10 $1 tmp_benchs/project_openfst.fst 0
+echo "RUST :"
+./target/release/rustfst-cli project $1 tmp_benchs/project_rustfst.fst --bench --n_warm_ups 3 --n_iters 10
+echo "==================================================================="
+echo -e "\n\n"
+
+echo "==================================================================="
+echo "BENCH ARCSORT"
+echo "C++ :"
+./openfst_benchmark/bench_arcsort 3 10 $1 tmp_benchs/arcsort_openfst.fst 0
+echo "RUST :"
+./target/release/rustfst-cli arcsort $1 tmp_benchs/arcsort_rustfst.fst --bench --n_warm_ups 3 --n_iters 10
+echo "==================================================================="
+echo -e "\n\n"
+
