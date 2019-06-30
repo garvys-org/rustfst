07a714b6f0ab8c4797d3413b3773172742f7356f
Alexandre Caulier <alexandre.caulier@orange.fr>
2019-06-30 20:56:55 +0200

    Bump to python3.6

--- .travis.sh
+++ .travis.sh
@@ -14,4 +14,4 @@ virtualenv venv3 -p python3.6
 . venv3/bin/activate
 pip install -e rustfst-python-bench
 # Run benches on a small FST to check that the script is working fine.
-python3 rustfst-python-bench/rustfst_python_bench/bench_all.py rustfst-tests-data/fst_003/raw_vector.fst report.md
+python rustfst-python-bench/rustfst_python_bench/bench_all.py rustfst-tests-data/fst_003/raw_vector.fst report.md
