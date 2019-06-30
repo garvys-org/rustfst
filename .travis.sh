b98e4eabed0a0077b1086e176660150c6f0e2509
Alexandre Caulier <alexandre.caulier@orange.fr>
2019-06-30 19:37:43 +0200

    Add bench python to CI

--- .travis.sh
+++ .travis.sh
@@ -9,3 +9,8 @@ cargo build --all
 cargo test --all
 cargo check --benches --all # running benches on travis is useless
 cargo doc --all
+
+virtualenv venv3 -p python3
+source venv3/bin/activate
+pip install -e rustfst-python-bench
+python3 rustfst-python-bench/rustfst_python_bench/bench_all.py rustfst-tests-data/fst_003/raw_vector.fst report.md
