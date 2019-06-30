3b90eddabebc2e3d95228b5307757df1e0b52f92
Alexandre Caulier <alexandre.caulier@orange.fr>
2019-06-30 20:56:14 +0200

    Bump to python3.6

--- .travis.sh
+++ .travis.sh
@@ -10,7 +10,7 @@ cargo test --all
 cargo check --benches --all # running benches on travis is useless
 cargo doc --all
 
-virtualenv venv3 -p python3
+virtualenv venv3 -p python3.6
 . venv3/bin/activate
 pip install -e rustfst-python-bench
 # Run benches on a small FST to check that the script is working fine.
