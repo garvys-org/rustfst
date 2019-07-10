21fd9f279539365c299d76cd4050acf989e034e3
Garvys <alexandre.caulier@orange.fr>
2019-07-10 23:41:43 +0200

    Add shortestpath to bench

--- rustfst-python-bench/rustfst_python_bench/algorithms/shortestpath.py
+++ rustfst-python-bench/rustfst_python_bench/algorithms/shortestpath.py
@@ -1,7 +1,11 @@
+from rustfst_python_bench.utils import check_fst_equals
+
+
 class ShortestPathAlgorithm:
 
-    def __init__(self):
-        pass
+    def __init__(self, nshortest=1, unique=False):
+        self.nshortest = nshortest
+        self.unique = unique
 
     @classmethod
     def openfst_cli(cls):
@@ -12,12 +16,26 @@ class ShortestPathAlgorithm:
         return "shortestpath"
 
     def get_openfst_bench_cli(self):
-        raise NotImplementedError
+        unique_s = "1" if self.unique else "0"
+        return "bench_shortestpath", [str(self.nshortest), unique_s]
 
     def get_cli_args(self):
-        return ""
+        r = []
+        if self.unique:
+            r.append("--unique")
+        r.append(f"--nshortest={self.nshortest}")
+        return " ".join(r)
 
     @classmethod
     def get_parameters(cls):
-        return [cls()]
+        return [
+            cls(nshortest=1, unique=False),
+            cls(nshortest=10, unique=False),
+            # cls(nshortest=10, unique=True)
+        ]
+
+    def check_correctness(self, path_res_openfst, path_res_rustfst):
+        # check_fst_equals(path_res_openfst, path_res_rustfst)
+        pass
+
 
