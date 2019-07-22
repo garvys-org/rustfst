6c07b3a80aba9a717eefed0e39da15d63f52a172
Garvys <alexandre.caulier@orange.fr>
2019-07-23 00:02:34 +0200

    Implement fst_convert to convert one fst type into another

---
+++ rustfst/src/algorithms/fst_convert.rs
@@ -0,0 +1,27 @@
+use crate::fst_traits::{ExpandedFst, MutableFst};
+
+pub fn fst_convert<F1, F2>(ifst: &F1) -> F2
+where
+    F1: ExpandedFst,
+    F2: MutableFst<W = F1::W> + ExpandedFst,
+{
+    let mut ofst = F2::new();
+    ofst.add_states(ifst.num_states());
+
+    if let Some(start) = ifst.start() {
+        // TODO: Need to implement a set_start_unchecked
+        ofst.set_start(start).unwrap();
+
+        for s in 0..ifst.num_states() {
+            for arc in unsafe { ifst.arcs_iter_unchecked(s) } {
+                unsafe { ofst.add_arc_unchecked(s, arc.clone()) };
+            }
+
+            if let Some(final_weight) = unsafe { ifst.final_weight_unchecked(s) } {
+                unsafe { ofst.set_final_unchecked(s, final_weight.clone()) };
+            }
+        }
+    }
+
+    ofst
+}
