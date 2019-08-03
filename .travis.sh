3ce4ed080f9fc5c2abf5b59db61ffe86beb1df47
Garvys <alexandre.caulier@orange.fr>
2019-08-03 14:25:36 +0200

    Fix ci

--- .travis.sh
+++ .travis.sh
@@ -10,6 +10,20 @@ cargo test --all
 cargo check --benches --all # running benches on travis is useless
 cargo doc --all
 
+wget https://github.com/SimonKagstrom/kcov/archive/master.tar.gz &&
+tar xzf master.tar.gz &&
+cd kcov-master &&
+mkdir build &&
+cd build &&
+cmake .. &&
+make &&
+make install DESTDIR=../../kcov-build &&
+cd ../.. &&
+rm -rf kcov-master &&
+for file in target/debug/rustfst-*; do [ -x "${file}" ] || continue; mkdir -p "target/cov/$(basename $file)"; ./kcov-build/usr/local/bin/kcov --exclude-pattern=/.cargo,/usr/lib --verify "target/cov/$(basename $file)" "$file"; done &&
+bash <(curl -s https://codecov.io/bash) &&
+echo "Uploaded code coverage"
+
 ./build_bench.sh
 python3 --version
 virtualenv venv3 -p python3.6
