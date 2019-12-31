900287cd72ef5de599cdee1bf90ba86c939d156e
Alexandre Caulier <alexandre.caulier@protonmail.com>
2019-12-31 11:29:23 +0100

    Add tests conversion fsts

--- rustfst/src/tests_openfst/fst_impls/const_fst.rs
+++ rustfst/src/tests_openfst/fst_impls/const_fst.rs
@@ -4,12 +4,27 @@ use crate::algorithms::fst_convert;
 use crate::fst_impls::{ConstFst, VectorFst};
 use crate::semirings::{Semiring, WeightQuantize};
 use crate::tests_openfst::FstTestData;
+use crate::SymbolTable;
+use crate::fst_traits::{MutableFst, Fst};
+use std::rc::Rc;
 
 pub fn test_const_fst_convert_convert<W>(test_data: &FstTestData<VectorFst<W>>) -> Fallible<()>
 where
     W: Semiring<Type = f32> + WeightQuantize,
 {
-    let raw_fst = test_data.raw.clone();
+    let mut raw_fst = test_data.raw.clone();
+
+    {
+        let mut isymt = SymbolTable::new();
+        isymt.add_symbol("a");
+        let mut osymt = SymbolTable::new();
+        osymt.add_symbol("b");
+        osymt.add_symbol("c");
+
+        raw_fst.set_input_symbols(Rc::new(isymt));
+        raw_fst.set_output_symbols(Rc::new(osymt));
+    }
+
     let const_fst: ConstFst<_> = raw_fst.clone().into();
     let pred_fst: VectorFst<_> = fst_convert(&const_fst);
     assert_eq!(
@@ -22,5 +37,29 @@ where
             "Convert VectorFst -> ConstFst -> VectorFst"
         )
     );
+
+    // Check symbol tables are still attached
+    {
+        let isymt = const_fst.input_symbols();
+        assert!(isymt.is_some());
+        let isymt = isymt.unwrap();
+        assert_eq!(isymt.len(), 2);
+
+        let osymt = const_fst.output_symbols();
+        assert!(osymt.is_some());
+        let osymt = osymt.unwrap();
+        assert_eq!(osymt.len(), 3);
+    }
+    {
+        let isymt = pred_fst.input_symbols();
+        assert!(isymt.is_some());
+        let isymt = isymt.unwrap();
+        assert_eq!(isymt.len(), 2);
+
+        let osymt = pred_fst.output_symbols();
+        assert!(osymt.is_some());
+        let osymt = osymt.unwrap();
+        assert_eq!(osymt.len(), 3);
+    }
     Ok(())
 }
