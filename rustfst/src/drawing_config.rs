df39d533b435169c21c47e65781ff50a803024ce
Alexandre Caulier <alexandre.caulier@protonmail.com>
2020-01-29 10:34:15 +0100

    DrawingConfig now takes some parameter as optional

--- rustfst/src/drawing_config.rs
+++ rustfst/src/drawing_config.rs
@@ -3,18 +3,16 @@
 pub struct DrawingConfig {
     /// Draw bottom-to-top instead of left-to-right.
     pub vertical: bool,
-    /// Set width.
-    pub width: f32,
-    /// Set height.
-    pub height: f32,
+    /// Set pair (width, height)
+    pub size: Option<(f32, f32)>,
     /// Set figure title.
     pub title: String,
     /// Portrait mode (def: landscape).
     pub portrait: bool,
     /// Set minimum separation between ranks (see dot documentation).
-    pub ranksep: f32,
+    pub ranksep: Option<f32>,
     /// Set minimum separation between nodes (see dot documentation).
-    pub nodesep: f32,
+    pub nodesep: Option<f32>,
     /// Set fontsize.
     pub fontsize: u32,
     /// Input in acceptor format.
@@ -27,12 +25,11 @@ impl Default for DrawingConfig {
     fn default() -> Self {
         Self {
             vertical: false,
-            width: 8.5,
-            height: 11.0,
+            size: None,
             title: "".to_string(),
             portrait: false,
-            ranksep: 0.40,
-            nodesep: 0.25,
+            ranksep: None,
+            nodesep: None,
             fontsize: 14,
             acceptor: false,
             show_weight_one: true,
