5fe0156dad9e63c41c6eed66ac20e5f978ed541f
Alexandre Caulier <alexandre.caulier@protonmail.com>
2020-02-07 13:43:49 +0100

    eps_loop SortedMatcher

--- rustfst/src/algorithms/matchers/mod.rs
+++ rustfst/src/algorithms/matchers/mod.rs
@@ -1,6 +1,12 @@
-use crate::fst_traits::ExpandedFst;
-use failure::Fallible;
 use std::slice::Iter as IterSlice;
+
+use failure::Fallible;
+use superslice::Ext;
+
+use crate::{Arc, NO_LABEL, NO_STATE_ID};
+use crate::{EPS_LABEL, Label, StateId};
+use crate::fst_traits::ExpandedFst;
+use crate::semirings::Semiring;
 
 #[derive(Copy, Debug, PartialOrd, PartialEq, Clone)]
 /// Specifies matcher action
@@ -16,8 +22,6 @@ enum MatchType {
     /// Otherwise, match unknown
     MatchUnknown,
 }
-use crate::semirings::Semiring;
-use crate::{Arc, Label, StateId};
 
 /// Matchers find and iterate through requested labels at FST states. In the
 /// simplest form, these are just some associative map or search keyed on labels.
@@ -30,14 +34,16 @@ pub trait Matcher<'a> {
     fn iter(&mut self, state: StateId, label: Label) -> Fallible<Self::Iter>;
 }
 
-struct SortedMatcher<'a, F: ExpandedFst> {
+struct SortedMatcher<'a, F: ExpandedFst>  {
     fst: &'a F,
     match_type: MatchType,
+    eps_loop: Arc<F::W>
 }
 
 impl<'a, F: ExpandedFst> SortedMatcher<'a, F> {
     pub fn new(fst: &'a F, match_type: MatchType) -> Self {
-        Self { fst, match_type }
+        // TODO: Add check label sorted
+        Self { fst, match_type, eps_loop: Arc::new(NO_LABEL, EPS_LABEL, F::W::one(), NO_STATE_ID) }
     }
 }
 
@@ -49,24 +55,38 @@ impl<'a, F: ExpandedFst> Matcher<'a> for SortedMatcher<'a, F> {
         Ok(IteratorSortedMatcher::new(
             self.fst.arcs_iter(state)?.collect(),
             label,
+            &self.eps_loop
         ))
     }
 }
 
-struct IteratorSortedMatcher<'a, W: Semiring> {
+struct IteratorSortedMatcher<'a, W: Semiring>{
     arcs: Vec<&'a Arc<W>>,
     match_label: Label,
     pos: usize,
+    current_loop: bool,
+    eps_loop: &'a Arc<W>,
 }
 
-use superslice::Ext;
 impl<'a, W: Semiring> IteratorSortedMatcher<'a, W> {
-    pub fn new(arcs: Vec<&'a Arc<W>>, match_label: Label) -> Self {
-        let pos = arcs.lower_bound_by(|x| x.ilabel.cmp(&match_label));
+    pub fn new(arcs: Vec<&'a Arc<W>>, match_label: Label, eps_loop: &'a Arc<W>) -> Self {
+
+        // If we have to match epsilon, an epsilon loop is added
+        let current_loop = match_label == EPS_LABEL;
+
+        // When matching epsilon, the first arc is supposed to be labeled as such
+        let pos = if current_loop {
+            0
+        } else {
+            arcs.lower_bound_by(|x| x.ilabel.cmp(&match_label))
+        };
+
         Self {
             arcs,
             match_label,
             pos,
+            current_loop,
+            eps_loop,
         }
     }
 }
@@ -75,6 +95,11 @@ impl<'a, W: Semiring> Iterator for IteratorSortedMatcher<'a, W> {
     type Item = &'a Arc<W>;
 
     fn next(&mut self) -> Option<Self::Item> {
+        if self.current_loop {
+            self.current_loop = false;
+            unimplemented!()
+//            return Some(&self.eps_loop)
+        }
         if let Some(arc) = self.arcs.get(self.pos) {
             if arc.ilabel == self.match_label {
                 self.pos += 1;
@@ -90,10 +115,11 @@ impl<'a, W: Semiring> Iterator for IteratorSortedMatcher<'a, W> {
 
 #[cfg(test)]
 mod tests {
-    use super::*;
     use crate::fst_impls::VectorFst;
     use crate::fst_traits::MutableFst;
     use crate::semirings::TropicalWeight;
+
+    use super::*;
 
     #[test]
     fn lol() -> Fallible<()> {
