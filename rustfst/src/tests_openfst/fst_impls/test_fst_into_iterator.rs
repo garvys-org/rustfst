e9b6cfabb298617409b870df0b1c4dc354e0381b
Alexandre Caulier <alexandre.caulier@protonmail.com>
2020-02-11 10:35:18 +0100

    Add tests

--- rustfst/src/tests_openfst/fst_impls/test_fst_into_iterator.rs
+++ rustfst/src/tests_openfst/fst_impls/test_fst_into_iterator.rs
@@ -2,11 +2,11 @@ use failure::Fallible;
 use itertools::Itertools;
 
 use crate::fst_impls::{ConstFst, VectorFst};
-use crate::fst_traits::ExpandedFst;
+use crate::fst_traits::{ExpandedFst, MutableFst};
 use crate::semirings::{SerializableSemiring, WeightQuantize};
 use crate::tests_openfst::FstTestData;
 
-fn do_test<F: ExpandedFst>(fst: F) -> Fallible<()> {
+fn do_test_fst_into_iterator<F: ExpandedFst>(fst: F) -> Fallible<()> {
     let mut fst_data_ref = vec![];
 
     for state in 0..fst.num_states() {
@@ -26,13 +26,58 @@ fn do_test<F: ExpandedFst>(fst: F) -> Fallible<()> {
     Ok(())
 }
 
+fn do_test_fst_iterator<F: ExpandedFst>(fst: &F) -> Fallible<()> {
+    let mut fst_data_ref = vec![];
+
+    for state in 0..fst.num_states() {
+        fst_data_ref.push((
+            state,
+            fst.arcs_iter(state)?.collect_vec(),
+            fst.final_weight(state)?,
+        ));
+    }
+
+    let mut fst_data = vec![];
+    for (state_id, arcs, final_weight) in fst.fst_iter() {
+        fst_data.push((state_id, arcs.collect_vec(), final_weight));
+    }
+    assert_eq!(fst_data, fst_data_ref);
+
+    Ok(())
+}
+
+fn do_test_fst_iterator_mut<F: MutableFst>(mut fst: F) -> Fallible<()> {
+    let mut fst_data_ref = vec![];
+
+    for state in 0..fst.num_states() {
+        fst_data_ref.push((
+            state,
+            fst.arcs_iter(state)?.cloned().collect_vec(),
+            fst.final_weight(state)?.cloned(),
+        ));
+    }
+
+    let mut fst_data = vec![];
+    for (state_id, arcs, final_weight) in fst.fst_iter_mut() {
+        fst_data.push((
+            state_id,
+            arcs.map(|e| e.clone()).collect_vec(),
+            final_weight.cloned(),
+        ));
+    }
+    assert_eq!(fst_data, fst_data_ref);
+
+    Ok(())
+}
+
 pub fn test_fst_into_iterator_const<W>(test_data: &FstTestData<VectorFst<W>>) -> Fallible<()>
 where
     W: SerializableSemiring + WeightQuantize + 'static,
 {
     let raw_fst: ConstFst<_> = test_data.raw.clone().into();
 
-    do_test(raw_fst)?;
+    do_test_fst_iterator(&raw_fst)?;
+    do_test_fst_into_iterator(raw_fst)?;
 
     Ok(())
 }
@@ -43,7 +88,9 @@ where
 {
     let raw_fst = test_data.raw.clone();
 
-    do_test(raw_fst)?;
+    do_test_fst_iterator(&raw_fst)?;
+    do_test_fst_into_iterator(raw_fst.clone())?;
+    do_test_fst_iterator_mut(raw_fst)?;
 
     Ok(())
 }
