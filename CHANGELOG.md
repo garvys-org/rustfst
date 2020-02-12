72aa192085f96b536ad86fc73e2029ae2e0bb7ca
Alexandre Caulier <alexandre.caulier@protonmail.com>
2020-02-12 17:08:35 +0100

    Update changelog

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -13,9 +13,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
     - `FstIterator` as a trait bound of `Fst`.
     - `FstIteratorMut` as a trait bound of `MutableFst`
 - Add `take_final_weight` and `take_final_weight_unchecked` to MutableFst API.
-- Add `add_super_final_state` algorithm
+- Add `add_super_final_state` algorithm.
 
 ### Changed
+- `fst_convert` now consumes its input. Use `fst_convert_from_ref` to pass a borrow.
 
 ## [0.5.0] - 2020-02-04
 
--- rustfst/src/tests_openfst/algorithms/fst_convert.rs
+++ rustfst/src/tests_openfst/algorithms/fst_convert.rs
@@ -1,6 +1,6 @@
 use std::fmt::Display;
 
-use crate::algorithms::fst_convert;
+use crate::algorithms::{fst_convert, fst_convert_from_ref};
 use crate::fst_traits::{AllocableFst, MutableFst, SerializableFst};
 use crate::semirings::{SerializableSemiring, WeaklyDivisibleSemiring};
 use crate::tests_openfst::FstTestData;
@@ -14,12 +14,20 @@ where
 {
     // Invert
     let fst = test_data.raw.clone();
-    let fst_converted: F = fst_convert(fst.clone());
+    let fst_converted_1: F = fst_convert(fst.clone());
     assert_eq!(
-        fst_converted,
+        fst_converted_1,
         fst,
         "{}",
-        error_message_fst!(fst_converted, fst, "Invert")
+        error_message_fst!(fst_converted_1, fst, "fst_convert")
+    );
+
+    let fst_converted_2: F = fst_convert_from_ref(&fst);
+    assert_eq!(
+        fst_converted_2,
+        fst,
+        "{}",
+        error_message_fst!(fst_converted_2, fst, "fst_convert")
     );
     Ok(())
 }
