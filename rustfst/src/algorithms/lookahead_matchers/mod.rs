218301040f380dc64b3414a634ff1a0f632c429f
Alexandre Caulier <alexandre.caulier@protonmail.com>
2020-03-10 10:25:13 +0100

    Reorganize lookahead matchers

--- rustfst/src/algorithms/lookahead_matchers/mod.rs
+++ rustfst/src/algorithms/lookahead_matchers/mod.rs
@@ -1,37 +1,6 @@
-use failure::Fallible;
-
-use crate::algorithms::matchers::{Matcher, MatcherFlags, MatchType};
+use crate::algorithms::matchers::Matcher;
 use crate::fst_traits::Fst;
 
+mod trivial_lookahead_matcher;
+
 pub trait LookaheadMatcher<'fst, F: Fst + 'fst>: Matcher<'fst, F> {}
-
-#[derive(Debug)]
-struct TrivialLookAheadMatcher<M> {
-    matcher: M
-}
-
-impl<'fst, F: Fst + 'fst, M: Matcher<'fst, F>> Matcher<'fst, F> for TrivialLookAheadMatcher<M> {
-    type Iter = M::Iter;
-
-    fn new(fst: &'fst F, match_type: MatchType) -> Fallible<Self>{
-        Ok(Self {
-            matcher: M::new(fst, match_type)?
-        })
-    }
-
-    fn iter(&self, state: usize, label: usize) -> Fallible<Self::Iter> {
-        self.matcher.iter(state, label)
-    }
-
-    fn final_weight(&self, state: usize) -> Fallible<Option<&'fst F::W>> {
-        self.matcher.final_weight(state)
-    }
-
-    fn match_type(&self) -> MatchType {
-        self.matcher.match_type()
-    }
-
-    fn flags(&self) -> MatcherFlags {
-        self.matcher.flags() | MatcherFlags::INPUT_LOOKAHEAD_MATCHER | MatcherFlags::OUTPUT_LOOKAHEAD_MATCHER
-    }
-}
