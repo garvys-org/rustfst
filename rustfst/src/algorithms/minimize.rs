ab8c09a9af24437989a3d65875a7de252f517331
Hubert de La Jonquière <hubert.delajonquiere@sonos.com>
2020-09-28 17:41:16 +0200

    Re-export and update documentation for acceptor_minimize

--- rustfst/src/algorithms/minimize.rs
+++ rustfst/src/algorithms/minimize.rs
@@ -109,6 +109,8 @@ where
 /// In place minimization for weighted final state acceptor. 
 /// If `allow_acyclic_minimization` is true and the input is acyclic, then a specific 
 /// minimization is applied. 
+///
+/// An error is returned if the input fst is not a weighted acceptor.
 pub fn acceptor_minimize<W: Semiring, F: MutableFst<W> + ExpandedFst<W>>(
     ifst: &mut F,
     allow_acyclic_minimization: bool,
