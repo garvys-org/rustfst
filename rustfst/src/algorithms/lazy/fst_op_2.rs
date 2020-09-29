640c5b7fdaf2323d29ed0e5f0f58d2b6c429186e
Hubert de La Jonquière <hubert.delajonquiere@sonos.com>
2020-09-29 14:06:12 +0200

    impl FstOp2 for Arc<FstOp2> and Rc<FstOp2>

--- rustfst/src/algorithms/lazy/fst_op_2.rs
+++ rustfst/src/algorithms/lazy/fst_op_2.rs
@@ -1,4 +1,7 @@
 use std::fmt::Debug;
+use std::ops::Deref;
+use std::rc::Rc;
+use std::sync::Arc;
 
 use anyhow::Result;
 
@@ -11,4 +14,34 @@ pub trait FstOp2<W: Semiring>: Debug {
     fn compute_trs_and_final_weight(&self, id: usize) -> Result<(TrsVec<W>, Option<W>)>;
 
     fn properties(&self) -> FstProperties;
+}
+
+impl<W: Semiring, F: FstOp2<W>> FstOp2<W> for Arc<F> {
+    fn compute_start(&self) -> Result<Option<StateId>> {
+        self.deref().compute_start()
+    }
+
+    fn compute_trs_and_final_weight(&self, id: usize) -> Result<(TrsVec<W>, Option<W>)> {
+        self.deref().compute_trs_and_final_weight(id)
+    }
+
+    // Computed at construction time
+    fn properties(&self) -> FstProperties {
+        self.deref().properties()
+    }
+}
+
+impl<W: Semiring, F: FstOp2<W>> FstOp2<W> for Rc<F> {
+    fn compute_start(&self) -> Result<Option<StateId>> {
+        self.deref().compute_start()
+    }
+
+    fn compute_trs_and_final_weight(&self, id: usize) -> Result<(TrsVec<W>, Option<W>)> {
+        self.deref().compute_trs_and_final_weight(id)
+    }
+
+    // Computed at construction time
+    fn properties(&self) -> FstProperties {
+        self.deref().properties()
+    }
 }
