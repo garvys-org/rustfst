fa7aea325dbe11695ce8c2163376aefcb449cf21
Emrick Sinitambirivoutin <emrick.sinitambirivoutin@sonos.com>
2022-01-12 23:00:36 +0100

    Add tests

---
+++ rustfst-python/test/test_fst.py
@@ -0,0 +1,20 @@
+from rustfst import Fst, Tr
+
+def test_small_fst():
+    fst = Fst()
+
+    # States
+    s1 = fst.add_state()
+    s2 = fst.add_state()
+
+    fst.set_start(s1)
+
+    # Trs
+    tr_1 = Tr(3, 5, 10.0, s2)
+    fst.add_tr(s1, tr_1)
+
+    assert fst.num_trs(s1) == 1
+
+    tr_2 = Tr(5, 7, 18.0, s2)
+    fst.add_tr(s1, tr_2)
+    assert fst.num_trs(s1) == 2
