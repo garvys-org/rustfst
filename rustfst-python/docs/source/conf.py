460c70953ad3203545e95df638dd4dadf35346dd
Garvys <alexandre.caulier@protonmail.com>
2022-05-05 13:55:34 +0200

    Fix doc

--- rustfst-python/docs/source/conf.py
+++ rustfst-python/docs/source/conf.py
@@ -28,6 +28,7 @@ author = 'Alexandre Caulier, Emrick Sinitambirivoutin'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinx.ext.autodoc'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
