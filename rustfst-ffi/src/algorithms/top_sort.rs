5531b72d943c44c296e4a71d40c59b66240e2290
Garvys <alexandre.caulier@protonmail.com>
2022-05-31 17:04:51 +0200

    Fix following reviews

--- rustfst-ffi/src/algorithms/top_sort.rs
+++ rustfst-ffi/src/algorithms/top_sort.rs
@@ -1,5 +1,6 @@
 use anyhow::anyhow;
 
+use crate::fst::as_mut_fst;
 use crate::fst::CFst;
 use crate::{get_mut, wrap, RUSTFST_FFI_RESULT};
 
@@ -11,9 +12,7 @@ use rustfst::semirings::TropicalWeight;
 pub extern "C" fn fst_top_sort(ptr: *mut CFst) -> RUSTFST_FFI_RESULT {
     wrap(|| {
         let fst = get_mut!(CFst, ptr);
-        let vec_fst: &mut VectorFst<TropicalWeight> = fst
-            .downcast_mut()
-            .ok_or_else(|| anyhow!("Could not downcast to vector FST"))?;
+        let vec_fst = as_mut_fst!(VectorFst<TropicalWeight>, fst);
         top_sort(vec_fst)?;
         Ok(())
     })
