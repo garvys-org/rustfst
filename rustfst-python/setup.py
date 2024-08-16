3799b81622a9ce7bb0ea5b6610313eaf70a4f886
Alexandre Caulier <alexandre.caulier@sonos.com>
2024-08-16 18:11:11 +0200

    Bump version to 1.1.2

--- rustfst-python/setup.py
+++ rustfst-python/setup.py
@@ -17,7 +17,7 @@ if readme_path.exists():
 
 PACKAGE_NAME = "rustfst-python"
 RUST_EXTENSION_NAME = "rustfst.dylib.dylib"
-VERSION = "1.1.1"
+VERSION = "1.1.2"
 REPO_ROOT_PATH = Path(__file__).resolve().parents[1]
 CARGO_ROOT_PATH = REPO_ROOT_PATH / "rustfst-ffi"
 CARGO_FILE_PATH = CARGO_ROOT_PATH / "Cargo.toml"
