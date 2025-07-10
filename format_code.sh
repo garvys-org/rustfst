6b619cd3f57390116f4f3b8983842ec0e044a1e5
Chloé Sekkat <chloe.sekkat@sonos.com>
2025-07-10 15:23:22 +0200

    add ruff in format_code.sh

--- format_code.sh
+++ format_code.sh
@@ -2,4 +2,5 @@
 set -e
 
 cargo fmt
+uv tool run ruff check . || { echo "Code not formatted! Please run 'uv tool run ruff format .'"; exit 1; }
 uv --directory rustfst-python tool run ruff format . && echo "Code formatted !"
